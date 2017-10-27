1071c0bf8b91f757dc76a451d1b7057db61817a2
dbugsy <dpbergquist@gmail.com>
2017-10-27 11:06:39 +0100

    Player cannot play on occupied position

--- __tests__/TicTacToe.test.js
+++ __tests__/TicTacToe.test.js
@@ -6,4 +6,10 @@ describe('TicTacToe', () => {
     expect(() => game.play('O', 0)).toThrow(new Error("Wrong player: it is X's turn"));
     expect(() => game.play('X', 0)).not.toThrow(new Error("Wrong player: it is X's turn"));
   });
+
+  it('ensures player cannot play on occupied position', () => {
+    const game = new TicTacToeGame();
+    game.play('X', 0);
+    expect(() => game.play('O', 0)).toThrow(new Error('Wrong move: position is already occupied'));
+  });
 });
--- src/TicTacToeGame.js
+++ src/TicTacToeGame.js
@@ -1,7 +1,18 @@
 class TicTacToeGame {
-  play(player) {
-    if (player === 'O')
-      {throw new Error("Wrong player: it is X's turn");}
+  constructor() {
+    this.turn = 'X';
+  }
+
+  play(player, position) {
+    if (player !== this.turn) {
+      throw new Error("Wrong player: it is X's turn");
+    }
+    if (this.position === position) {
+      throw new Error('Wrong move: position is already occupied');
+    }
+
+    this.position = position;
+    if (player === 'X') { this.turn = 'O'; }
   }
 }
 
