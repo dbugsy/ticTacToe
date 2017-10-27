396a11610dc13c2bdd06e4b1e34e22ca387b3093
dbugsy <dpbergquist@gmail.com>
2017-10-27 11:45:02 +0100

    refactor to add game instanciation to beforeEach

--- __tests__/TicTacToe.test.js
+++ __tests__/TicTacToe.test.js
@@ -1,26 +1,27 @@
 const TicTacToeGame = require('../src/TicTacToeGame');
 
 describe('TicTacToe', () => {
+  let game;
+
+  beforeEach(() => {
+    game = new TicTacToeGame();
+  });
+
   it('ensures X always plays first', () => {
-    const game = new TicTacToeGame();
     expect(() => game.play('O', 0)).toThrow(new Error("Wrong player: it is X's turn"));
     expect(() => game.play('X', 0)).not.toThrow(new Error("Wrong player: it is X's turn"));
   });
 
   it('ensures player cannot play on occupied position', () => {
-    const game = new TicTacToeGame();
     game.play('X', 0);
     expect(() => game.play('O', 0)).toThrow(new Error('Wrong move: position is already occupied'));
   });
 
-
   it('is not a draw unless all positions are occupied', () => {
-    const game = new TicTacToeGame();
     expect(game.isDraw()).toEqual(false);
   });
 
   it('is a draw when all 9 positions are occupied', () => {
-    const game = new TicTacToeGame();
     game.play('X', 0);
     game.play('O', 1);
     game.play('X', 2);
